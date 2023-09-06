19323df4e1fb61ddce1bd9d5c004d9d81fa09eb9
journey <admin@NJ-MQK5FHW0N4.local>
2023-09-06 18:49:44 +0800

    show nft

--- src/app/components/stake.tsx
+++ src/app/components/stake.tsx
@@ -2,25 +2,38 @@
 import { useEffect, useState } from 'react'
 import { alchemyI } from '../config'
 import { useAccount } from 'wagmi'
-
 interface Props {
   title: string
   contractAddress: string
 }
 
+interface NftInfo {
+  nftName?: string
+  tokenId: string
+  image?: string
+}
+
 async function queryUserCollections(address: string, collectionAddress: string) {
   const data = await alchemyI.nft.getNftsForOwner(address, { contractAddresses: [collectionAddress] })
-  console.log('data', data)
+  return data.ownedNfts.map(item => {
+    return {
+      nftName: item.contract.name,
+      tokenId: item.tokenId,
+      image: item.media[0].gateway,
+    }
+  })
 }
 
 export default function Stake(props: Props) {
   const { address, isConnected } = useAccount()
-  const [tokens, setTokens] = useState([])
+  const [tokens, setTokens] = useState<NftInfo[]>([])
 
   useEffect(() => {
-    console.log('address', address)
     if (address) {
       queryUserCollections(address, props.contractAddress)
+        .then(list => {
+          setTokens(list)
+        })
     }
   }, [address, props.contractAddress])
 
@@ -28,6 +41,25 @@ export default function Stake(props: Props) {
     <div>Connect First!</div>
   )
   return (
-    <p className=''>{ props.title }</p>
+    <div className='w-full mx-4 bg-white mt-4 rounded-lg p-4'>
+      <p className='text-2xl font-bold text-center mb-4'>{ props.title }</p>
+      {tokens.length > 0 && <div className=''>
+        { tokens.map(t => (
+          <div key={t.tokenId} className="flex items-center">
+            <img className="w-16 h-16 rounded-full mr-4" src={t.image} alt="" />
+            <div className="text-left">
+              <p className="font-medium">
+                {`${t.nftName} #${t.tokenId}`}
+              </p>
+              <div className='flex'>
+                <div className="text-sky-500">claim</div>
+                <div className="text-slate-700">unstake</div>
+              </div>
+            </div>
+          </div>
+        )) }
+      </div>
+      }  
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { ssrTool } from './config'
 export default function Home() {
   return (
     <div>
-      <div className='container mx-auto flex justify-between items-center p-4 h-16'>
+      <div className='container mx-auto flex justify-between items-center p-4 h-16 bg-white'>
         <div className='flex items-center'>
           <img src="https://app.mami.network/_next/image?url=%2Fimg%2Flogo.png&w=128&q=75" alt="" width="45px" height="45px" />
           <p className='font-bold text-2xl pl-4 hidden'>Little Mami Lp Stake</p>
@@ -16,9 +16,7 @@ export default function Home() {
 
       <div className='flex flex-wrap mt-4'>
         <Tip />
-        <div className='w-full px-4'>
-          <Stake title='Ssr tool质押' contractAddress={ssrTool.goerli} />
-        </div>
+        <Stake title='Ssr tool质押' contractAddress={ssrTool.goerli} />
       </div>
     </div>
   )
